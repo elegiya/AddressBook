46ad13df06ffa4bed0a8b682aae3df3c2b1f2e65
Marina Gaponova <marina.a.gaponova@gmail.com>
2015-09-26 20:53:22 +0300

    Initial commit

---
+++ AddressBook/AddressBook/AddressBook.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using AddressBook.WinPhone.Resources;
+
+namespace AddressBook.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
